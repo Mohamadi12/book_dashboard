43c6678ea62b375fd187d385beb18b72ec6de12c
Nana Mohamadi <adilnana78@gmail.com>
2025-01-12 15:03:05 +0100

    Neon and Drizzle Setup

--- src/app/(rout)/page.tsx
+++ src/app/(rout)/page.tsx
@@ -1,17 +1,23 @@
-import BookList from '@/components/BookList'
-import BookOverview from '@/components/BookOverview'
-import { sampleBooks } from '@/constants'
-import React from 'react'
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import React from "react";
 
-const Home = () =>(
-  <>
-   <BookOverview {...sampleBooks[0]}/>
-   <BookList 
+const Home = async() => {
+  const results = await db.select().from(users)
+
+  return (
+    <>
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
         title="Latest Books"
         books={sampleBooks}
         containerClassName="mt-28"
-   />
-  </>
-)
+      />
+    </>
+  );
+};
 
-export default Home
+export default Home;
