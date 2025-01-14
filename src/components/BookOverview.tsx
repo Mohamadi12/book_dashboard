f947846de978ce874e06c9ea9aa66aea4f08bacf
Nana Mohamadi <adilnana78@gmail.com>
2025-01-14 19:50:38 +0100

    Deployment and Production Error Fix

--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -29,11 +29,9 @@ const BookOverview = async ({
     .where(eq(users.id, userId))
     .limit(1);
 
-  if (!user) return null;
-
   //APPROVED from to database manually
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message:
       availableCopies <= 0
         ? "Book is not available"
@@ -71,7 +69,13 @@ const BookOverview = async ({
 
         <p className="book-description">{description}</p>
 
-        <BorrowBook bookId={id} userId={userId} borrowingEligibility={borrowingEligibility}/>
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
 
       <div className="relative flex flex-1 justify-center">
